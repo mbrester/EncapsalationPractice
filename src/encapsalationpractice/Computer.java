915ffc56b05341f193228f3b4348d21e8f4d9062
Mitch <Mitch@192.168.1.110>
2015-02-05 09:59:56 -0600

    first commit, Adding project to git

---
+++ src/encapsalationpractice/Computer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encapsalationpractice;
+
+/**
+ *
+ * @author Mitch
+ */
+public class Computer {
+    Motherboard mobo = new Motherboard();
+    RAM rAM = new RAM();
+    HardDrive hardDrv
+}
